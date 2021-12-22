4598e4e77de81331821b5112ee47906696664f68
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2021-12-22 13:58:35 -0500

    renamed schema

---
+++ models/product.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const Product = new Schema(
+  {
+    price: { type: String, required: true },
+    item: { type: String, required: true },
+    size: { type: String, required: true },
+    style: { type: String, required: true },
+    color: { type: String, required: true },
+    brand: { type: String, required: true },
+    description: { type: String, required: true },
+    imgURL: { type: String, required: true },
+  },
+  { timestamp: true }
+)
+
+export default mongoose.model('products', Products)
