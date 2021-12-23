3cf79e8cf4757759983dfa96a363d66a5f7cd659
Kaheem Freeman <kashfr@gmail.com>
2021-12-23 09:53:47 -0500

    changed the name of a file

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,12 @@
-import { Router } from 'express';
-import * as controllers from '../controllers/product.js';
+import { Router } from "express";
+import * as controllers from "../controllers/products.js";
 
+const router = Router();
 
-const router = Router()
+router.get("/products", controllers.getProducts);
+router.get("/products/:id", controllers.getProduct);
+router.post("/products", controllers.createProduct);
+router.put("/products/:id", controllers.editProduct);
+router.delete("/products/:id", controllers.deleteProduct);
 
-router.get('/products', controllers.getProducts)
-router.get('/products/:id', controllers.getProduct)
-router.post('/products', controllers.createProduct)
-router.put('/products/:id', controllers.editProduct)
-router.delete('/products/:id', controllers.deleteProduct)
-
-export default router
+export default router;
