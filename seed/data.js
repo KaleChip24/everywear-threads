12da1226057304558c2543003d87a2f81e104c37
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2021-12-23 10:39:54 -0500

    edited User import in data.js

--- seed/data.js
+++ seed/data.js
@@ -1,6 +1,6 @@
 import db from '../db/connection.js';
 import Product from '../models/product.js';
-import User from '../models/user'
+import User from '../models/user.js'
 import bcrypt from 'bcrypt'
 
 const insertData = async () => {
