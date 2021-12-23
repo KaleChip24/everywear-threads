f8800371a92b9f831607cffb46d06a111247c1b0
Zhao Zhong <zhzhong@sas.upenn.edu>
2021-12-23 11:45:26 -0700

    adding more frontend

---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import Nav from '../Nav/Nav';
+import Footer from '../Footer/Footer';
+
+const Layout = (props) => (
+	<div className='layout'>
+		<Nav />
+		<div className='layout-children'>{props.children}</div>
+		<Footer />
+	</div>
+);
+
+export default Layout;
