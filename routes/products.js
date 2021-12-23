edd3fee4f95a0e94bdd1f54a0e7ffcabada033a6
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2021-12-23 10:58:12 -0500

    impoted restrict in product.js routes

--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import * as controllers from "../controllers/products.js";
+import restrict from '../helpers/restrict.js'
 
 const router = Router();
 
 router.get("/products", controllers.getProducts);
 router.get("/products/:id", controllers.getProduct);
-router.post("/products", controllers.createProduct);
-router.put("/products/:id", controllers.editProduct);
-router.delete("/products/:id", controllers.deleteProduct);
+router.post("/products", restrict, controllers.createProduct);
+router.put("/products/:id", restrict, controllers.editProduct);
+router.delete("/products/:id", restrict, controllers.deleteProduct);
 
 export default router;
