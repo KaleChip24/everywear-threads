ca3b951c5bef8c3f21a5e1b5447219f1e0f6fd51
Zhao Zhong <zhong@lavabit.com>
2022-01-03 12:15:23 -0700

    making nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,27 +1,11 @@
 import './Nav.css';
 import { NavLink } from 'react-router-dom';
+import ResponsiveAppBar from "./ResponsiveAppBar";
 
 const Nav = () => {
 	return (
 		<nav>
-			<div className='nav'>
-				<NavLink className='logo' to='/'>
-					ProductsApp
-				</NavLink>
-				<div className='links'>
-					<NavLink className='link' to='/'>
-						Products
-					</NavLink>
-					<NavLink className='link' to='/add-product'>
-						Add Product
-          </NavLink>
-          <NavLink className="link" to="/add-product">Add Product</NavLink>
-        <NavLink className="link" to="/sign-out">Sign Out</NavLink>
-        <NavLink className="link" to="/sign-up">Sign Up</NavLink>
-        <NavLink className="link" to="/sign-in">Sign In</NavLink>
-
-				</div>
-			</div>
+			<ResponsiveAppBar />
 		</nav>
 	);
 };
--- client/src/screens/Products/Products.js
+++ client/src/screens/Products/Products.js
@@ -1,41 +1,40 @@
 import React from 'react'
 import {useState, useEffect} from 'react'
 import { getProducts } from '../../services/products'
-// import { Layout } from '../../components'
+import Layout from '../../components/Layout/Layout';
 
 export default function Products() {
+	const [products, setProducts] = useState([]);
 
-  const [products, setProducts] = useState([])
+	useEffect(() => {
+		const fetchProducts = async () => {
+			const allProducts = await getProducts();
+			setProducts(allProducts);
+			console.log(setProducts);
+		};
+		fetchProducts();
+	}, []);
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const allProducts = await getProducts()
-      setProducts(allProducts)
-      console.log(setProducts)
-    }
-    fetchProducts()
-  }, [])
-
-  return (
-    // <Layout>
-		<div className='products'>
-			{products.map((product, index) => {
-				return (
-					<div>
-						<div className='productImage'>
-							{product.imgURL.map((image) => {
-								return <img src={image}></img>;
-							})}
+	return (
+		<Layout>
+			<div className='products'>
+				{products.map((product, index) => {
+					return (
+						<div>
+							<div className='productImage'>
+								{product.imgURL.map((image) => {
+									return <img src={image}></img>;
+								})}
+							</div>
+							<h2>{product.item}</h2>
+							<h3>By {product.brand}</h3>
+							<h5>{product.price}</h5>
+							<h5>{product.color}</h5>
+							<p>{product.description}</p>
 						</div>
-						<h2>{product.item}</h2>
-						<h3>By {product.brand}</h3>
-						<h5>{product.price}</h5>
-						<h5>{product.color}</h5>
-						<p>{product.description}</p>
-					</div>
-				);
-			})}
-      </div>
-  //  </Layout>
-  );
+					);
+				})}
+			</div>
+		</Layout>
+	);
 }
