09162811cd50dc0e1b9aeb6674d28803e23500e4
Kaheem Freeman <kashfr@gmail.com>
2022-01-03 14:45:46 -0500

    removed duplicate add-product

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,29 +1,33 @@
-import './Nav.css';
-import { NavLink } from 'react-router-dom';
+import "./Nav.css";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
-	return (
-		<nav>
-			<div className='nav'>
-				<NavLink className='logo' to='/'>
-					ProductsApp
-				</NavLink>
-				<div className='links'>
-					<NavLink className='link' to='/'>
-						Products
-					</NavLink>
-					<NavLink className='link' to='/add-product'>
-						Add Product
+  return (
+    <nav>
+      <div className="nav">
+        <NavLink className="logo" to="/">
+          Everywear Threads
+        </NavLink>
+        <div className="links">
+          <NavLink className="link" to="/">
+            Products
           </NavLink>
-          <NavLink className="link" to="/add-product">Add Product</NavLink>
-        <NavLink className="link" to="/sign-out">Sign Out</NavLink>
-        <NavLink className="link" to="/sign-up">Sign Up</NavLink>
-        <NavLink className="link" to="/sign-in">Sign In</NavLink>
-
-				</div>
-			</div>
-		</nav>
-	);
+          <NavLink className="link" to="/add-product">
+            Add Product
+          </NavLink>
+          <NavLink className="link" to="/sign-out">
+            Sign Out
+          </NavLink>
+          <NavLink className="link" to="/sign-up">
+            Sign Up
+          </NavLink>
+          <NavLink className="link" to="/sign-in">
+            Sign In
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
