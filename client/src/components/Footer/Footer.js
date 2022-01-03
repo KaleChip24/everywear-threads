679b0d35f9b6c6c50a68a9837ddeb73e35f92916
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2022-01-03 13:05:29 -0800

    lavell has been added to the footer, added a rel to anchor for security

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -11,21 +11,61 @@ const Footer = () => {
           <a
             className="gitHub"
             href="https://github.com/KaleChip24"
-            target="_blank">
+            target="_blank"
+            rel="noreferrer">
             <img
               src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/github_fpykxh.png"
-              class="icons"
+              className="icons"
               alt="GitHub" />
+          </a>
+          <a className="linkdIn"
+            href="https://www.linkedin.com/in/oharakale/"
+            target="_blank"
+            rel="noreferrer">
+            <img
+              src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/linkedin_vnvo6s.png"
+              className="icons"
+              alt="linkedin" />
           </a>
           <p>Zhao</p>
           <a
             className="gitHub"
             href="https://github.com/iamzhaozhong"
-            target="_blank">
+            target="_blank"
+            rel="noreferrer">
+            <img
+              src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/github_fpykxh.png"
+              className="icons"
+              alt="GitHub" />
+          </a>
+          <a className="linkdIn"
+            href="https://www.linkedin.com/in/junzhaozhongliu/"
+            target="_blank"
+            rel="noreferrer">
+            <img
+              src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/linkedin_vnvo6s.png"
+              className="icons"
+              alt="linkedin" />
+          </a>
+          <p>Lavell</p>
+          <a
+            className="gitHub"
+            href="https://github.com/lavell25"
+            target="_blank"
+            rel="noreferrer">
             <img
               src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/github_fpykxh.png"
               class="icons"
               alt="GitHub" />
+          </a>
+          <a className="linkdIn"
+            href="https://www.linkedin.com/in/lavellHCollins/"
+            target="_blank"
+            rel="noreferrer">
+            <img
+              src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/linkedin_vnvo6s.png"
+              className="icons"
+              alt="linkedin" />
           </a>
           <small>&copy; GA SEI Velvet Worms 2022</small>
         </div>
