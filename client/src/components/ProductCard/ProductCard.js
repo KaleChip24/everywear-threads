c958ead90119573d9a907e1942261cc1f925a872
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2022-01-04 12:15:44 -0500

    images and button added to landing page

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -5,12 +5,11 @@ const ProductCard = (props) => {
   return (
     <div className="product-card">
       <Link className="card" to={`/products/${props._id}`}>
-      {props.imgURL.map((image) => {
+        {props.imgURL.map((image) => {
           return (
             <img className="product-image" src={image} alt={props.item}></img>
           );
         })}
-        <div>View</div>
       </Link>
     </div>
   );
--- client/src/screens/Home/Home.js
+++ client/src/screens/Home/Home.js
@@ -1,11 +1,18 @@
 import Layout from "../../components/Layout/Layout";
 import "./Home.css";
 import ProductCards from "../../components/ProductCards/ProductCards";
+import { Link } from "react-router-dom";
 
 const Home = (props) => {
   return (
     <Layout user={props.user}>
+      <img src="https://i.ibb.co/6n5WQRz/NewStyle.png" alt="new style"></img>
+      <img src="https://i.ibb.co/9cgZVrW/New-Arrival.png" alt="new arrival"></img>
+      <img src="https://i.ibb.co/Ytfk965/New-Season.png" alt="new season"></img>
       <div className="home">
+        <Link to={`/products`}>
+          <button>Shop Now</button>
+        </Link>
         <ProductCards />
       </div>
     </Layout>
