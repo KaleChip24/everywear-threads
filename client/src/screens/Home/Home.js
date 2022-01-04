711befbf7fd5db8fcdb8d4253b5236f91cfdef3a
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2022-01-04 15:07:52 -0500

    updated product useState in ProductCreate.js

--- client/src/screens/Home/Home.js
+++ client/src/screens/Home/Home.js
@@ -6,14 +6,17 @@ import { Link } from "react-router-dom";
 const Home = (props) => {
   return (
     <Layout user={props.user}>
-      <img src="https://i.ibb.co/6n5WQRz/NewStyle.png" alt="new style"></img>
-      <img src="https://i.ibb.co/9cgZVrW/New-Arrival.png" alt="new arrival"></img>
-      <img src="https://i.ibb.co/Ytfk965/New-Season.png" alt="new season"></img>
-      <div className="home">
-        <Link to={`/products`}>
-          <button>Shop Now</button>
-        </Link>
-        <ProductCards />
+      <div className="laning-page">
+        <img className="landing-img" src="https://i.ibb.co/6n5WQRz/NewStyle.png" alt="new style"></img>
+        <img className="landing-img" src="https://i.ibb.co/9cgZVrW/New-Arrival.png" alt="new arrival"></img>
+        <img className="landing-img" src="https://i.ibb.co/Ytfk965/New-Season.png" alt="new season"></img>
+        <p>Fashion marketpalce app centered around inclusivity and commpunity. But. Sell. Discover preloved threads for everyone, Everwear.</p>
+        <div className="home">
+          <Link to={`/products`}>
+            <button>Shop Now</button>
+          </Link>
+          <ProductCards />
+        </div>
       </div>
     </Layout>
   );
