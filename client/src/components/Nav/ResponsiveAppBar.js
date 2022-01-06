33993ea00776fc37c8f3e97de655b35f06db8b41
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2022-01-05 20:55:38 -0500

    fixed the logo pathway in the nav bar to redirect you to the landing screen

--- client/src/components/Nav/ResponsiveAppBar.js
+++ client/src/components/Nav/ResponsiveAppBar.js
@@ -21,171 +21,171 @@ const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 
 const ResponsiveAppBar = () => {
-	const [anchorElNav, setAnchorElNav] = React.useState(null);
-	const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-	const handleOpenNavMenu = (event) => {
-		setAnchorElNav(event.currentTarget);
-	};
-	const handleOpenUserMenu = (event) => {
-		setAnchorElUser(event.currentTarget);
-	};
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-	const handleCloseNavMenu = () => {
-		setAnchorElNav(null);
-	};
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-	const handleCloseUserMenu = () => {
-		setAnchorElUser(null);
-	};
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-	return (
-		<AppBar
-			position='static'
-			sx={{
-				backgroundColor: '#c8e7ff',
-				maxHeight: '20vh',
-			}}>
-			<Container maxWidth='xl'>
-				<Toolbar disableGutters>
-					{/* //the logo */}
-					<Container maxWidth='sm'>
-						<Box>
-							<CardActions>
-								<NavLink to='/Home'>
-									<CardMedia
-										style={{
-											height: 'auto',
-											width: 'auto',
-											maxHeight: '20vh',
-										}}
-										component='img'
-										image='https://raw.githubusercontent.com/KaleChip24/everywear-threads/zz-dev/assets/logo/logo.png'
-										alt='logo'
-									/>
-								</NavLink>
-							</CardActions>
-						</Box>
-					</Container>
+  return (
+    <AppBar
+      position='static'
+      sx={{
+        backgroundColor: '#c8e7ff',
+        maxHeight: '20vh',
+      }}>
+      <Container maxWidth='xl'>
+        <Toolbar disableGutters>
+          {/* //the logo */}
+          <Container maxWidth='sm'>
+            <Box>
+              <CardActions>
+                <NavLink to='/'>
+                  <CardMedia
+                    style={{
+                      height: 'auto',
+                      width: 'auto',
+                      maxHeight: '20vh',
+                    }}
+                    component='img'
+                    image='https://raw.githubusercontent.com/KaleChip24/everywear-threads/zz-dev/assets/logo/logo.png'
+                    alt='logo'
+                  />
+                </NavLink>
+              </CardActions>
+            </Box>
+          </Container>
 
-					<Box
-						sx={{
-							flexGrow: 1,
-							display: { xs: 'flex', md: 'none' },
-						}}>
-						<IconButton
-							size='large'
-							aria-label='account of current user'
-							aria-controls='menu-appbar'
-							aria-haspopup='true'
-							onClick={handleOpenNavMenu}
-							color='inherit'>
-							<MenuIcon />
-						</IconButton>
-						<Menu
-							id='menu-appbar'
-							anchorEl={anchorElNav}
-							anchorOrigin={{
-								vertical: 'bottom',
-								horizontal: 'left',
-							}}
-							keepMounted
-							transformOrigin={{
-								vertical: 'top',
-								horizontal: 'left',
-							}}
-							open={Boolean(anchorElNav)}
-							onClose={handleCloseNavMenu}
-							sx={{
-								display: { xs: 'block', md: 'none' },
-							}}>
-							{pages.map((page) => (
-								<CardActions>
-									<MenuItem
-										key={page}
-										onClick={handleCloseNavMenu}>
-										<NavLink to={`/${page}`}>
-											<Typography
-												textAlign='center'
-												sx={{
-													display: {
-														xs: 'block',
-														md: 'none',
-													},
-												}}>
-												{page}
-											</Typography>
-										</NavLink>
-									</MenuItem>
-								</CardActions>
-							))}
-						</Menu>
-					</Box>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: 'flex', md: 'none' },
+            }}>
+            <IconButton
+              size='large'
+              aria-label='account of current user'
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              onClick={handleOpenNavMenu}
+              color='inherit'>
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id='menu-appbar'
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}>
+              {pages.map((page) => (
+                <CardActions>
+                  <MenuItem
+                    key={page}
+                    onClick={handleCloseNavMenu}>
+                    <NavLink to={`/${page}`}>
+                      <Typography
+                        textAlign='center'
+                        sx={{
+                          display: {
+                            xs: 'block',
+                            md: 'none',
+                          },
+                        }}>
+                        {page}
+                      </Typography>
+                    </NavLink>
+                  </MenuItem>
+                </CardActions>
+              ))}
+            </Menu>
+          </Box>
 
-					<Box
-						sx={{
-							flexGrow: 1,
-							display: { xs: 'none', md: 'flex' },
-						}}>
-						{pages.map((page) => (
-							<CardActions>
-								<Button
-									component={NavLink}
-									to={`/${page}`}
-									variant='outlined'
-									key={page}
-									onClick={handleCloseNavMenu}
-									sx={{
-										color: 'black',
-										display: 'block',
-										margin: 'auto',
-									}}>
-									{page}
-								</Button>
-							</CardActions>
-						))}
-					</Box>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: 'none', md: 'flex' },
+            }}>
+            {pages.map((page) => (
+              <CardActions>
+                <Button
+                  component={NavLink}
+                  to={`/${page}`}
+                  variant='outlined'
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{
+                    color: 'black',
+                    display: 'block',
+                    margin: 'auto',
+                  }}>
+                  {page}
+                </Button>
+              </CardActions>
+            ))}
+          </Box>
 
-					<Box sx={{ flexGrow: 0 }}>
-						<Tooltip title='Open settings'>
-							<IconButton
-								onClick={handleOpenUserMenu}
-								sx={{ p: 0 }}>
-								<Avatar
-									alt='user'
-									src='https://raw.githubusercontent.com/KaleChip24/everywear-threads/zz-dev/assets/icons/nav.png'
-								/>
-							</IconButton>
-						</Tooltip>
-						<Menu
-							sx={{ mt: '45px' }}
-							id='menu-appbar'
-							anchorEl={anchorElUser}
-							anchorOrigin={{
-								vertical: 'top',
-								horizontal: 'right',
-							}}
-							keepMounted
-							transformOrigin={{
-								vertical: 'top',
-								horizontal: 'right',
-							}}
-							open={Boolean(anchorElUser)}
-							onClose={handleCloseUserMenu}>
-							{settings.map((setting) => (
-								<MenuItem
-									key={setting}
-									onClick={handleCloseNavMenu}>
-									<Typography textAlign='center'>
-										{setting}
-									</Typography>
-								</MenuItem>
-							))}
-						</Menu>
-					</Box>
-				</Toolbar>
-			</Container>
-		</AppBar>
-	);
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title='Open settings'>
+              <IconButton
+                onClick={handleOpenUserMenu}
+                sx={{ p: 0 }}>
+                <Avatar
+                  alt='user'
+                  src='https://raw.githubusercontent.com/KaleChip24/everywear-threads/zz-dev/assets/icons/nav.png'
+                />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id='menu-appbar'
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}>
+              {settings.map((setting) => (
+                <MenuItem
+                  key={setting}
+                  onClick={handleCloseNavMenu}>
+                  <Typography textAlign='center'>
+                    {setting}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 export default ResponsiveAppBar;
