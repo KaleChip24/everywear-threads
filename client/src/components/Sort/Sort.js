9920defdc28129441bc53c30f77e7d42b5fe17b8
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2022-01-07 12:55:34 -0500

    hid the a-z sorting for the time being, not being used now

--- client/src/components/Sort/Sort.js
+++ client/src/components/Sort/Sort.js
@@ -9,12 +9,12 @@ const Sort = (props) => {
     <form className="sort-container" onSubmit={props.handleSubmit}>
       <label htmlFor="sort">SORT BY:</label>
       <select className="sort" onChange={handleSort}>
-        <option className="option" value="name-ascending">
+        {/* <option className="option" value="name-ascending">
           &nbsp; Alphabetically, A-Z &nbsp;
         </option>
         <option value="name-descending">
           &nbsp; Alphabetically, Z-A &nbsp;
-        </option>
+        </option> */}
         <option value="price-ascending">
           &nbsp; Price, low to high &nbsp;
         </option>
